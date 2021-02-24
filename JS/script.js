3b31499ebd2f8daa56a5275e3de78cc9bfdeff0b
Александр Ершов <sanya_evr@mail.ru>
2021-02-25 00:34:29 +0600

    lesson 7

--- JS/script.js
+++ JS/script.js
@@ -11,17 +11,38 @@ let question = [question1, question2, question3, question4];
 //счетчик правильных ответов - используется в функции getAnswer
 let counter = 0;
 
-function getAnswer() {
+// Функция showResults перебирает массив путем запуска каждого вопроса в модальное окно с полем для ввода ответа,
+// ответ поступает в переменную i, если ответ строго равен ответу из массива, то счетчик увеличивается на единицу.
+// Если нет - ничего не происходит.
+// И так по каждому вопросу. После последнего вопроса перебор закончен, а функция showResults продлжает:
+// в переменную answer передается ссылка на тег с классом .correctAnswer из HTML и затем текст из этого тега заменяется
+// на кол-во верных ответов.
+function showResults() {
   question.forEach((item) => {
   let i = prompt(item.question);
 	if (item.correctAnswer.toLowerCase() === i.toLowerCase()) {
   	counter++;
   }
 })
-}
-
-getAnswer();
-
-//вывод количества правильных ответов в HTML
 let answer = document.querySelector('.correctAnswer');
 answer.innerHTML = 'Правильных ответов: ' + counter;
+}
+
+//здесь создается переменная со ссылкой на тег с классом .runTest из html
+//затем по клику на этот тег срабатывает функция
+const showResultsButton = document.querySelector('.runTest');
+showResultsButton.addEventListener('click', () => {
+  showResults();
+});
+
+//это пока не понятно для чего сделали - видимо, дальше будем дорабатывать
+const previousButton = document.querySelector('.previous-button');
+const nextButton = document.querySelector('.next-button');
+
+previousButton.addEventListener('click', (event) => {
+  console.log('Предыдущий слайд');
+});
+
+nextButton.addEventListener('click', (event) => {
+  console.log('Следующий слайд');
+});
